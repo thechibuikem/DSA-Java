3b3b066b93aa037dc24997682d113306ca2eb41a
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-09-18 00:27:43 +0100

    chore: fix quadratic time

--- timeComplexity/quadraticTime.java
+++ timeComplexity/quadraticTime.java
@@ -1,12 +1,20 @@
 package timeComplexity;
 
 public class quadraticTime {
-    int [ ] list  = {"1", "2", "3", "4", "5"};
+    String [] list  = {"1", "2", "3", "4", "5"};
     
-    public void printAllPossibleOrderedPairs(list){
-        for (a : list) {
-            for (b : list) {
-                system.out.println(a + " " + b);
+    public void printAllPossibleOrderedPairs(){
+        for (String a : list) {
+            for (String b : list) {
+                System.out.println(a + " " + b);
     };
         };
+}
+
+public static void main(String[] args) {
+    quadraticTime obj = new quadraticTime();
+    obj.printAllPossibleOrderedPairs();
+}
+
+
 };
