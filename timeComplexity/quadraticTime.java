15cc6107f0d58e6b661bc2e5785a02b154e0c52c
Chukwuemeka David <136800623+thechibuikem@users.noreply.github.com>
2026-09-17 17:57:02 +0100

    Correct syntax in quadraticTime class
    Fix syntax errors in printAllPossibleOrderedPairs method.

--- timeComplexity/quadraticTime.java
+++ timeComplexity/quadraticTime.java
@@ -2,10 +2,11 @@ package timeComplexity;
 
 public class quadraticTime {
     int [ ] list  = {"1", "2", "3", "4", "5"};
+    
     public void printAllPossibleOrderedPairs(list){
         for (a : list) {
             for (b : list) {
-                system.out.println(a + " " + b)
+                system.out.println(a + " " + b);
     };
         };
-}
+};
